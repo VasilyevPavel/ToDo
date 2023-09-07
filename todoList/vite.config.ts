cc7167c16916db1490f8065c4df503811bab8983
VasilyevPavel <iam100500@ya.ru>
2023-09-07 15:33:36 +0300

    Initial commit

--- todoList/vite.config.ts
+++ todoList/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ToDo_List/',
+  base: '/ToDo_Mindbox/',
   plugins: [react()],
 });
